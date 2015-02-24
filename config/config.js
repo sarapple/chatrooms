03fe12b70340af52775d2dbc63d5872f26d19025
Sara Wong <swong2@wellesley.edu>
2015-02-23 18:20:32 -0800

    pushing to app with updated URI

--- config/config.js
+++ config/config.js
@@ -13,7 +13,7 @@ var notifier = {
 	};
 module.exports = {	//self creates db
 		development: {
-			db: 'heroku_app34287904:S4r4ppl3!.mongolab.com:47901/heroku_app34287904',
+			db: 'sarapple:Bu7bp9ug.mongolab.com:47901/heroku_app34287904',
 			root: rootPath,
 			notifier: notifier,
 			app: {
@@ -21,7 +21,7 @@ module.exports = {	//self creates db
 			}
 		},
 		test: {
-			db: 'heroku_app34287904:S4r4ppl3!.mongolab.com:47901/heroku_app34287904', 
+			db: 'sarapple:Bu7bp9ug.mongolab.com:47901/heroku_app34287904', 
 			root: rootPath,
 			notifier: notifier,
 			app:{
